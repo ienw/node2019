d03b51d99bfc1ae0f475fe4a9269be984294b0b2
ienw <ienw@metropolia.fi>
2019-11-15 12:29:31 +0200

    public folder

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/',(req,res) => {
         res.send("Hi from my express server")
     }
 );
 
-app.listen(3000);
+app.get('/demo',(req, res) => {
+        console.log("request", req);
+	res.send('demo')
+    }
+);
+
+app.listen(3000, () => {
+	console.log('listening from port 3000');
+});
